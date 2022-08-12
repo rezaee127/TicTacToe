8b18af4735d248c1a05436d635d2a969766d4d0a
razi <rezaie127@yahoo.com>
2022-08-12 09:44:29 +0430

    change colors

--- app/src/main/java/com/example/hw6_2/MainActivity.kt
+++ app/src/main/java/com/example/hw6_2/MainActivity.kt
@@ -7,11 +7,12 @@ import android.widget.Button
 import com.example.hw6_2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding:ActivityMainBinding
-    var flag=true
-    var count=0
+    private lateinit var binding:ActivityMainBinding
+    private var flag=true
+    private var count=0
     @SuppressLint("SetTextI18n")
     var listOfButtons=ArrayList<Button>()
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -37,6 +38,7 @@ class MainActivity : AppCompatActivity() {
     }
 
 
+    @SuppressLint("SetTextI18n")
     private fun reset(){
         for (button in listOfButtons){
             button.text=""
@@ -59,11 +61,11 @@ class MainActivity : AppCompatActivity() {
         flag=!flag
         button.isClickable=false
         if(winner()==1){
-            listOfButtons.forEach{it.isEnabled=false}
+            listOfButtons.forEach{it.isClickable=false}
         }
         if(binding.textView.text=="Draw" && count==9){
             binding.textView.text="No Winner"
-            listOfButtons.forEach{it.isEnabled=false}
+            listOfButtons.forEach{it.isClickable=false}
         }
     }
 
@@ -73,35 +75,35 @@ class MainActivity : AppCompatActivity() {
     fun winner():Int {
         if (count >= 5) {
             if (binding.button1.text == binding.button2.text && binding.button1.text == binding.button3.text && binding.button3.text != "") {
-                binding.textView.text = "player ${binding.button1.text.toString()} winner"
+                binding.textView.text = "player ${binding.button1.text} winner"
                 return 1
             }
             if (binding.button4.text == binding.button5.text && binding.button4.text == binding.button6.text && binding.button6.text != "") {
-                binding.textView.text = "player ${binding.button4.text.toString()} winner"
+                binding.textView.text = "player ${binding.button4.text} winner"
                 return 1
             }
             if (binding.button7.text == binding.button8.text && binding.button7.text == binding.button9.text && binding.button9.text != "") {
-                binding.textView.text = "player ${binding.button7.text.toString()} winner"
+                binding.textView.text = "player ${binding.button7.text} winner"
                 return 1
             }
             if (binding.button1.text == binding.button4.text && binding.button1.text == binding.button7.text && binding.button7.text != "") {
-                binding.textView.text = "player ${binding.button1.text.toString()} winner"
+                binding.textView.text = "player ${binding.button1.text} winner"
                 return 1
             }
             if (binding.button2.text == binding.button5.text && binding.button2.text == binding.button8.text && binding.button8.text != "") {
-                binding.textView.text = "player ${binding.button2.text.toString()} winner"
+                binding.textView.text = "player ${binding.button2.text} winner"
                 return 1
             }
             if (binding.button3.text == binding.button6.text && binding.button3.text == binding.button9.text && binding.button9.text != "") {
-                binding.textView.text = "player ${binding.button3.text.toString()} winner"
+                binding.textView.text = "player ${binding.button3.text} winner"
                 return 1
             }
             if (binding.button1.text == binding.button5.text && binding.button1.text == binding.button9.text && binding.button9.text != "") {
-                binding.textView.text = "player ${binding.button1.text.toString()} winner"
+                binding.textView.text = "player ${binding.button1.text} winner"
                 return 1
             }
             if (binding.button3.text == binding.button5.text && binding.button3.text == binding.button7.text && binding.button7.text != "") {
-                binding.textView.text = "player ${binding.button3.text.toString()} winner"
+                binding.textView.text = "player ${binding.button3.text} winner"
                 return 1
             }
         }
